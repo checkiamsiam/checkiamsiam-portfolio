7be23cb66005f944eb097472693e3e3cf14c47e3
checkiamsiam <issiam02415@gmail.com>
2022-06-10 00:34:28 +0600

    email js applier

--- src/pages/Home/Contact/ContactSection.js
+++ src/pages/Home/Contact/ContactSection.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import { BiSend } from "react-icons/bi";
 import { MdEmail } from "react-icons/md";
 import { RiWhatsappFill } from "react-icons/ri";
@@ -7,6 +8,19 @@ import { BsTwitter } from "react-icons/bs";
 import { AiFillInstagram } from "react-icons/ai";
 
 const ContactSection = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_0o8844l', 'template_u2g9ofm', form.current, 'llUopf-9itfABWZ3z')
+      .then((result) => {
+          alert('message sent');
+          e.target.reset()
+      }, (error) => {
+        alert('error');
+      });
+  };
   return (
     <div className='py-10 container mx-auto px-5'>
       <h1 className='text-accent text-3xl title-font text-center mb-5'>Feel Free To Me</h1>
@@ -43,14 +57,14 @@ const ContactSection = () => {
           </div>
         </div>
         <div>
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <div className='flex md:flex-row flex-col gap-5 mb-5'>
-              <input type="text" placeholder="YOUR NAME" class="input bg-neutral w-full  focus:outline-primary focus:outline-1 rounded-full text-base-100 " />
-              <input type="text" placeholder="EMAIL" class="input bg-neutral  w-full  focus:outline-primary focus:outline-1 rounded-full text-base-100 " />
-              <input type="text" placeholder="SUBJECT" class="input bg-neutral focus:outline-primary focus:outline-1 w-full rounded-full text-base-100 " />
+              <input name='name' type="text" placeholder="YOUR NAME" class="input bg-neutral w-full  focus:outline-primary focus:outline-1 rounded-full text-base-100 " />
+              <input name='email' type="text" placeholder="EMAIL" class="input bg-neutral  w-full  focus:outline-primary focus:outline-1 rounded-full text-base-100 " />
+              <input name='subject' type="text" placeholder="SUBJECT" class="input bg-neutral focus:outline-primary focus:outline-1 w-full rounded-full text-base-100 " />
             </div>
             <div>
-              <textarea class="textarea w-full h-32 bg-neutral text-base-100  focus:outline-primary focus:outline-1 rounded-2xl" placeholder="MESSAGE"></textarea>
+              <textarea name='message' class="textarea w-full h-32 bg-neutral text-base-100  focus:outline-primary focus:outline-1 rounded-2xl" placeholder="MESSAGE"></textarea>
             </div>
             <div>
               <button type='submit' class="dedcription-btn">
